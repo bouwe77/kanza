8443d5c534049b87d9cf07b593bb1a0a9cf8abfa
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2026-02-04 20:43:36 +0100

    fix: do not bundle react/jsx-runtime

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.jsx'),
-      name: 'Kanza',
-      fileName: 'kanza',
-      formats: ['es']
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "Kanza",
+      fileName: "kanza",
+      formats: ["es"],
     },
     rollupOptions: {
-      external: ['react', 'react-dom']
-    }
-  }
-})
+      external: ["react", "react-dom", "react/jsx-runtime"],
+    },
+  },
+});
